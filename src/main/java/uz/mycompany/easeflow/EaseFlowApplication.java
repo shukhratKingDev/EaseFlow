ca4e3a075b399a74de3298dfbe30d57e226b10d8
Shukhratjon Rayimjonov <shrayimjonov@cdti.uz>
2024-01-20 14:57:33 +0500

    initial commit

---
+++ src/main/java/uz/mycompany/easeflow/EaseFlowApplication.java
@@ -0,0 +1,13 @@
+package uz.mycompany.easeflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EaseFlowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EaseFlowApplication.class, args);
+    }
+
+}
---
+++ src/test/java/uz/mycompany/easeflow/EaseFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.mycompany.easeflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EaseFlowApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
